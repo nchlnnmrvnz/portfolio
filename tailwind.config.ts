f94e1679d3db3a235a7d21ae5fe42aa86ce4ee10
nchlnnmrvnz <avanez.nichole@gmail.com>
2023-12-06 23:34:30 +0800

    fix: UPDATE global.css and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,28 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'dark-gray': '#2A2A2E',
+        'light-gray': '#34353A',
+        'pink': '#F4ACB7',
       },
+
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat' : ['Montserrat', 'sans-serif']
+      },
+
+      fontSize: {
+        base: '1.25rem',
+        lg: '1.5rem',
+        xl: '4rem'
+      }
     },
   },
   plugins: [],
