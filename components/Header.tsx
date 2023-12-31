afe9b8aafd1c3c827655f6bd4348f638019e4267
nchlnnmrvnz <avanez.nichole@gmail.com>
2023-12-31 22:51:30 +0800

    fix: UPDATE styling of header component for array parameter

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC<HeaderProps> = ({ textProp }) => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center text-xl font-bold font-poppins">
           {Array.isArray(textProp) ? (
-               textProp.map((text, index) => <h1 key={index}>{text}</h1>)
+               textProp.map((text, index) => <h1 key={index} className="h-screen">{text}</h1>)
           ) : (
                <h1>{textProp}</h1>
           )}
